65172e0d1ef564f5bd2e28a72ade81af9b31d503
manel sepulveda <manuel_sep@hotmail.com>
2018-09-12 17:29:45 +0100

    cl tags doing shit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,13 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  Rails.application.configure do
+  # ...
+  config.action_mailer.delivery_method     = :postmark
+  config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
+  config.action_mailer.default_url_options = { host: "boostyourstudy.herokuapp.com" }
+  # or your custom domain name eg. "www.yourdomain.com"
+end
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
