d213c2dbb62e6ac17dd5d5267b4f7d8bd046f984
manel sepulveda <manuel_sep@hotmail.com>
2018-09-13 02:07:31 +0100

    done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Show full error reports.
